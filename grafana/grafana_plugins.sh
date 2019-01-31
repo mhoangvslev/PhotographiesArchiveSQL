a3935ade58f7eaaff85c764a1c0d37cabdae20a8
Minh-Hoang DANG <minhhoangdang@hotmail.com>
2019-01-31 17:38:41 +0100

    Deuxieme prototype
    - Test avec Worldmap plugin
    - Un script pour installer les plugins

---
+++ grafana/grafana_plugins.sh
@@ -0,0 +1,4 @@
+sudo grafana-cli plugins install grafana-worldmap-panel
+sudo grafana-cli plugins install grafana-piechart-panel
+sudo grafana-cli plugins install natel-plotly-panel
+sudo service grafana-server restart
