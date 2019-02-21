feb4dedac467ce8f7cb46bfb7bbd894d92ea4b2f
Minh-Hoang DANG <minhhoangdang@hotmail.com>
2019-02-21 18:09:54 +0100

    Hotfix

--- www/archivistus/src/Form/DocumentFormType.php
+++ www/archivistus/src/Form/DocumentFormType.php
@@ -4,10 +4,6 @@ namespace App\Form;
 
 use App\Entity\Document;
 use Symfony\Component\Form\AbstractType;
-<<<<<<< Updated upstream
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-=======
->>>>>>> Stashed changes
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -24,15 +20,6 @@ class DocumentFormType extends AbstractType
             ->add('n_v')
             ->add('c_g')
             ->add('idville')
-<<<<<<< Updated upstream
-            ->add('iddate')
-            ->add('idoeuvre')
-            ->add('idsujet')
-            ->add('idico')
-            ->add('idcliche')
-            ->add('Insert/Update', SubmitType::class, ['label' => 'Insert/Update'])
-            ->add('Delete', SubmitType::class, ['label' => 'Delete'])
-=======
             ->add('idico')
             ->add('iddate')
             ->add('idoeuvre')
@@ -40,7 +27,6 @@ class DocumentFormType extends AbstractType
             ->add('idcliche')
             ->add("Insert/Update", SubmitType::class, ['label' => 'Insert/Update'])
             ->add("Delete", SubmitType::class, ['label' => 'Delete'])
->>>>>>> Stashed changes
         ;
     }
 
